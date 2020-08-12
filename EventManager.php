78232c413dde152d042c8074e7e132e4a2805427
Patrick Bogdan <raQai@users.noreply.github.com>
2020-08-12 20:32:14 +0200

    Setup basic wordpress plugin.

---
+++ EventManager.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Event Manager
+ * Description: Simple EventManager Plugin
+ * Author: Patrick Bogdan
+ * Version: 0.0.1
+ */
+defined('ABSPATH') or die('Nope!');
+
+if (!defined('WPINC')) {
+    die;
+}
