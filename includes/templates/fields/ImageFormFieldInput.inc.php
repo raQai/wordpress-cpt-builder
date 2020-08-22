020858d3cd9bc65afb7fdb762eb7866e0cb8dfe2
Patrick Bogdan <raQai@users.noreply.github.com>
2020-08-22 22:36:09 +0200

    Use buttons instead of inputs for image selection field input templates.

--- includes/templates/fields/ImageFormFieldInput.inc.php
+++ includes/templates/fields/ImageFormFieldInput.inc.php
@@ -15,5 +15,5 @@ defined('ABSPATH') or die('Nope!');
 <div id="<?php echo $render_object->id; ?>_media-container" style="cursor:pointer">
     <?php if ($render_object->has_image_set) echo $render_object->attachment_image; ?>
 </div>
-<input type="button" class="button button-secondary button-small <?php echo $render_object->id; ?>_media-button <?php if ($render_object->has_image_set) echo 'hidden'; ?>" id="<?php echo $render_object->id; ?>_media-button" name="<?php echo $render_object->id; ?>_media-button" value="<?php _e('Set Image'); ?>" />
-<input type="button" class="button button-secondary button-small button-link-delete <?php echo $render_object->id; ?>_media-remove <?php if (!$render_object->has_image_set) echo 'hidden'; ?>" id="<?php echo $render_object->id; ?>_media-remove" name="<?php echo $render_object->id; ?>_media-remove" value="<?php _e('Remove Image'); ?>" />
+<button type="button" class="button button-secondary button-small <?php echo $render_object->id; ?>_media-button <?php if ($render_object->has_image_set) echo 'hidden'; ?>" id="<?php echo $render_object->id; ?>_media-button" name="<?php echo $render_object->id; ?>_media-button"><?php _e('Set Image'); ?></button>
+<button type="button" class="button button-secondary button-small button-link-delete <?php echo $render_object->id; ?>_media-remove <?php if (!$render_object->has_image_set) echo 'hidden'; ?>" id="<?php echo $render_object->id; ?>_media-remove" name="<?php echo $render_object->id; ?>_media-remove"><?php _e('Remove Image'); ?></button>
