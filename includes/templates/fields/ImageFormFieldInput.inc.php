4c6362bf0fe9d823965a4a138798d8599aead6ce
Patrick Bogdan <raQai@users.noreply.github.com>
2020-08-22 22:35:34 +0200

    Change cursor to pointer on media image container for image selection fields.

--- includes/templates/fields/ImageFormFieldInput.inc.php
+++ includes/templates/fields/ImageFormFieldInput.inc.php
@@ -12,7 +12,7 @@ defined('ABSPATH') or die('Nope!');
             array_keys($render_object->input_attributes),
             $render_object->input_attributes
         )); ?> class="<?php echo $render_object->id; ?>_media-url">
-<div id="<?php echo $render_object->id; ?>_media-container">
+<div id="<?php echo $render_object->id; ?>_media-container" style="cursor:pointer">
     <?php if ($render_object->has_image_set) echo $render_object->attachment_image; ?>
 </div>
 <input type="button" class="button button-secondary button-small <?php echo $render_object->id; ?>_media-button <?php if ($render_object->has_image_set) echo 'hidden'; ?>" id="<?php echo $render_object->id; ?>_media-button" name="<?php echo $render_object->id; ?>_media-button" value="<?php _e('Set Image'); ?>" />
