ed6d0df4ec7301a36cfdb901bda5b3d8added205
Patrick Bogdan <raQai@users.noreply.github.com>
2020-08-23 18:32:31 +0200

    Update php doc for TaxonomyFieldInterface.

--- classes/taxonomy/fields/TaxonomyFieldInterface.class.php
+++ classes/taxonomy/fields/TaxonomyFieldInterface.class.php
@@ -9,23 +9,23 @@ interface TaxonomyFieldInterface
     /**
      * Initialize all necessary actions and filters for this field.
      *
-     * @param slug (string) The taxonomy slug
+     * @param string $slug The taxonomy slug
      */
     public function init($slug);
 
     /**
-     * Simple getter for the term metadata. returns a default value if $term_id is null
-     * or false if no value was set
+     * Simple getter for the term metadata.
+     * May return a default value if $term_id is null or false if no value was set
      *
-     * @param term_id (int) Term ID.
+     * @param int $term_id Term ID.
      */
     public function getValue($term_id = null);
 
     /**
      * Fires after a new term in a specific taxonomy is created, and after the term cache has been cleaned.
      * 
-     * @param term_id (int) Term ID.
-     * @param tt_id (int) Term taxonomy ID.
+     * @param int $term_id Term ID.
+     * @param int $tt_id Term taxonomy ID.
      * @see https://developer.wordpress.org/reference/hooks/created_taxonomy/
      */
     public function saveValue($term_id, $tt_id);
@@ -33,8 +33,8 @@ interface TaxonomyFieldInterface
     /**
      * Fires after a term for a specific taxonomy has been updated, and the term cache has been cleaned.
      *
-     * @param term_id (int) Term ID.
-     * @param tt_id (int) Term taxonomy ID.
+     * @param int $term_id Term ID.
+     * @param int $tt_id Term taxonomy ID.
      * @see https://developer.wordpress.org/reference/hooks/edited_taxonomy/
      */
     public function updateValue($term_id, $tt_id);
@@ -42,7 +42,7 @@ interface TaxonomyFieldInterface
     /**
      * Fires after the Add Term form fields.
      *
-     * @param slug (string) The taxonomy slug.
+     * @param string $slug The taxonomy slug.
      * @see https://developer.wordpress.org/reference/hooks/taxonomy_add_form_fields/
      */
     public function renderFormField($slug);
@@ -50,8 +50,8 @@ interface TaxonomyFieldInterface
     /**
      * Fires after the Edit Term form fields are displayed.
      *
-     * @param term (WP_Term) Current taxonomy term object.
-     * @param slug (string) Current taxonomy slug.
+     * @param WP_Term $term Current taxonomy term object.
+     * @param string $slug Current taxonomy slug.
      * @see https://developer.wordpress.org/reference/hooks/taxonomy_edit_form_fields/
      */
     public function renderEditFormField($term, $slug);
@@ -59,7 +59,7 @@ interface TaxonomyFieldInterface
     /**
      * Filters the column headers for a list table on a specific screen.
      *
-     * @param columns (string[]) The column header labels keyed by column ID.
+     * @param string[] $columns The column header labels keyed by column ID.
      * @see https://developer.wordpress.org/reference/hooks/manage_screen-id_columns/
      */
     public function addTableColumn($columns);
@@ -67,9 +67,9 @@ interface TaxonomyFieldInterface
     /**
      * Filters the displayed columns in the terms list table.
      * 
-     * @param content (string) Blank string.
-     * @param column_name (string) Name of the column.
-     * @param term_id (int) Term ID. 
+     * @param string $content Blank string.
+     * @param string $column_name Name of the column.
+     * @param int $term_id Term ID.
      * @see https://developer.wordpress.org/reference/hooks/manage_this-screen-taxonomy_custom_column/
      */
     public function addTableContent($content, $column_name, $term_id);
