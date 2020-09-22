e74f9430d8fab4a28ec8da52b8ec4de9226fc533
Patrick Bogdan <raQai@users.noreply.github.com>
2020-09-22 19:43:22 +0200

    [CPTBuilder] Set version to beta1.

--- CPTBuilder.php
+++ CPTBuilder.php
@@ -10,7 +10,7 @@
  * Plugin Name: BIWS CPT Builder
  * Description: Builder Plugin to generate custom post types
  * Author: Patrick Bogdan
- * Version: 1.0.0
+ * Version: 1.0.0-beta1
  * License: GPL v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
